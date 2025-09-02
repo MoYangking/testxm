a4793415cc08d5e9201b8d51b04f0061c42132d7
MoYang <72334065+MoYangking@users.noreply.github.com>
2025-09-02 17:02:29 +0800

    Delete python directory

--- python/astrbot/core/pipeline/context.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from astrbot.core.config.astrbot_config import AstrBotConfig
-from astrbot.core.star import PluginManager
-
-
-@dataclass
-class PipelineContext:
-    astrbot_config: AstrBotConfig
-    plugin_manager: PluginManager
--- python/astrbot/core/provider/__init__.py
+++
@@ -1,5 +0,0 @@
-from .provider import Provider, Personality, STTProvider
-
-from .entites import ProviderMetaData
-
-__all__ = ["Provider", "Personality", "ProviderMetaData", "STTProvider"]
